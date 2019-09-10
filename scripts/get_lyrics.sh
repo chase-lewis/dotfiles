1129646fa80d5f67850ff969ca5b4e485bee94e4
Chase Lewis <chaselewis@chaselewis-macbookpro.roam.corp.google.com>
2019-09-09 21:07:22 -0700

    Delete irrelevant files

--- scripts/get_lyrics.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-song=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata 2>/dev/null | sed -n '/title/{n;p}' | cut -d '"' -f 2 | sed 's/ /%20/g')
-artist=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:org.mpris.MediaPlayer2.Player string:Metadata 2>/dev/null | sed -n '/artist/{n;n;p}' | cut -d '"' -f 2 | sed 's/ /%20/g')
-url="http://lyric-api.herokuapp.com/api/find/$artist/$song"
-lyrics=$(curl -s $url | jq -r ".lyric")
-printf "%s"  "$(curl -s $url | jq -r ".lyric")"
--- scripts/wallpaper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export DISPLAY=:0.0
-export XAUTHORITY=/home/chase/.Xauthority
-
-sleep 1
-nitrogen --restore
-
-# feh --bg-fill "/home/chase/Pictures/wallpaper.jpg"
